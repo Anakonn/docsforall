4a7203a8e6ebee2608556c5dc575e29510af51d7
ego <ego@anakonn.com>
2025-07-06 06:10:53 +0900

    Astro 설정 파일에 사이트 URL 추가, Starlight 통합에 다국어 지원 및 로고 설정 추가. Sitemap 통합 및 여러 SVG 아이콘 파일 추가.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,49 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
+
+const site = 'https://docsforall.com';
 
 // https://astro.build/config
 export default defineConfig({
+  site,
   integrations: [
     starlight({
       title: 'My Docs',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      locales: {
+        root: {
+          label: '한국어',
+          lang: 'ko',
+        },
+      },
+      head: [
+        // Add ICO favicon fallback for Safari.
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'icon',
+            href: '/favicon.ico',
+            sizes: '32x32',
+          },
+        },
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2130210715518535',
+            crossorigin: 'anonymous',
+          },
+        },
+      ],
+      // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      logo: {
+        light: './src/assets/docsfa-logo.svg',
+        dark: './src/assets/docsfa-logo-dark.svg',
+        alt: 'Docs for All Logo',
+        replacesTitle: true,
+      },
       sidebar: [
         {
           label: 'Guides',
@@ -31,6 +65,7 @@ export default defineConfig({
         },
       ],
     }),
+    sitemap(),
   ],
 
   adapter: cloudflare(),
